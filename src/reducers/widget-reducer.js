d6463e959c939d0e3f3974cc55fdcd8b64e904e9
sravanikumar <kumar.sra@northeastern.edu>
2021-03-19 00:47:30 -0400

    added all action types for widget reducer

---
+++ src/reducers/widget-reducer.js
@@ -0,0 +1,62 @@
+const initialState = {
+    widgets: []
+}
+
+const widgetReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "CREATE_WIDGET":
+            return {
+                ...state,
+                widgets: [
+                    ...state.widgets,
+                    action.widget
+                ]
+            }
+        case "DELETE_WIDGET":
+            return {
+                ...state,
+                widgets: state.widgets.filter(widget => {
+                    if(widget._id !== action.widgetToDelete._id) {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+            }
+        case "UPDATE_WIDGET":
+            return {
+                ...state,
+                widgets: state.widgets.map(widget => {
+                    if(widget._id === action.updatedWidget._id) {
+                        return action.updatedWidget
+                    } else {
+                        return widget
+                    }
+                })
+            }
+        case "FIND_ALL_WIDGETS_FOR_TOPIC":
+            return {
+                ...state,
+                widgets: action.widgets
+            }
+        case "FIND_ALL_WIDGETS":
+            return {
+                ...state,
+                widgets: action.widgets
+            }
+        case "FIND_WIDGET":
+            return {
+                ...state.widgets.map(widget => {
+                        if (widget.id === action.widget.id) {
+                            return action.widget
+                        }
+                    }
+                )
+            }
+
+        default:
+            return state
+    }
+}
+
+export default widgetReducer
