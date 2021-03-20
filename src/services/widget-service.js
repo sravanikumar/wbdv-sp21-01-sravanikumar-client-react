711fbc04b0062dca4cd4cd4d17d74ef6c27887ed
sravanikumar <kumar.sra@northeastern.edu>
2021-03-19 23:50:15 -0400

    updated api url to heroku from localhost

--- src/services/widget-service.js
+++ src/services/widget-service.js
@@ -1,5 +1,5 @@
-const TOPICS_URL = "http://localhost:8080/api/topics"
-const WIDGETS_URL = "http://localhost:8080/api/widgets"
+const TOPICS_URL = "https://wbdv-sp21-sravani-server-java.herokuapp.com/api/topics"//"http://localhost:8080/api/topics"
+const WIDGETS_URL = "https://wbdv-sp21-sravani-server-java.herokuapp.com/api/widgets" //"http://localhost:8080/api/widgets"
 
 export const createWidget = (topicId, widget) =>
     fetch(`${TOPICS_URL}/${topicId}/widgets`, {
