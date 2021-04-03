a915ab13e54a17f5630d335ee0dc89e9465b2910
sravanikumar <kumar.sra@northeastern.edu>
2021-04-02 22:35:02 -0400

    implemented editing list widget

--- src/components/widgets/list-widget.js
+++ src/components/widgets/list-widget.js
@@ -1,21 +1,24 @@
 import React, {useState} from 'react'
 import {Link} from "react-router-dom";
+import TypeSelection from "./type-selection";
 
 const ListWidget = ({widget, updateWidget, deleteWidget, to, back}) => {
     const [editing, setEditing] = useState(false)
     const [widgetCache, setWidgetCache] = useState(widget)
+    // const [ordered, setOrdered] = useState(Boolean(widgetCache.ordered))
+    console.log(widgetCache)
     return (
         <div>
             {
                 !editing &&
                 <>
                     {
-                        widget && widget.text && widget.ordered &&
+                        widgetCache && widgetCache.text && widgetCache.ordered &&
                         <div className="row">
                             <div className="col-11">
                                 <ol>
                                     {
-                                        widget.text.split("\n").map(item => {
+                                        widgetCache.text.split("\n").map(item => {
                                             return(
                                                 <li>{item}</li>
                                             )
@@ -32,12 +35,12 @@ const ListWidget = ({widget, updateWidget, deleteWidget, to, back}) => {
                     }
 
                     {
-                        widget && widget.text && !widget.ordered &&
+                        widgetCache && widgetCache.text && !widgetCache.ordered &&
                         <div className="row">
                             <div className="col-11">
                                 <ul>
                                     {
-                                        widget.text.split("\n").map(item => {
+                                        widgetCache.text.split("\n").map(item => {
                                             return(
                                                 <li>{item}</li>
                                             )
@@ -57,13 +60,61 @@ const ListWidget = ({widget, updateWidget, deleteWidget, to, back}) => {
 
             {
                 editing &&
-                <div>
-                    <input type="checkbox"/> Ordered
-                    <br/>
-                    List Items
-                    <textarea rows={10} value={widget.text} className="form-control">
+                <div className="row">
+                    <div className="col-11">
+                        <TypeSelection widgetCache={widgetCache} setWidgetCache={setWidgetCache}/>
+                        <br/>
+                        <span>
+                            List Items
+                            <span className="float-right">
+                                {
+                                    widgetCache.ordered &&
+                                    <>
+                                        <input type="checkbox"
+                                            onChange={(e => {
+                                                // setOrdered(!ordered)
+                                                setWidgetCache({
+                                                    ...widgetCache,
+                                                    ordered: !widgetCache.ordered
+                                                })
+                                            })}
+                                        checked/> Ordered
+                                    </>
+                                }
+                                {
+                                    !widgetCache.ordered &&
+                                    <>
+                                        <input type="checkbox"
+                                               onChange={(e => {
+                                                   // setOrdered(!ordered)
+                                                   setWidgetCache({
+                                                       ...widgetCache,
+                                                       ordered: !widgetCache.ordered
+                                                   })
+                                               })}
+                                        />Ordered
+                                    </>
+                                }
+                            </span>
+                        </span>
+                        <textarea rows={10}
+                                  value={widgetCache.text}
+                                  onChange={(e) => setWidgetCache({...widgetCache, text: e.target.value})}
+                                  className="form-control"></textarea>
+                    </div>
+                    <div className="col-1">
+                        <Link to={back}>
+                            <i onClick={() => {
+                                setEditing(false)
+                                updateWidget(widgetCache)
+                            }} className="fas fa-check float-right"></i>
 
-                    </textarea>
+                            <i onClick={() => {
+                                setEditing(false)
+                                deleteWidget(widget)
+                            }} className="fas fa-trash float-right"></i>
+                        </Link>
+                    </div>
                 </div>
             }
         </div>
