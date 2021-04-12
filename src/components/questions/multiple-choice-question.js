5215fa41eece75b3ed96933f5552f99d39fb7b44
sravanikumar <kumar.sra@northeastern.edu>
2021-04-12 19:53:39 -0400

    implemented multiple choice question grading with highlights

--- src/components/questions/multiple-choice-question.js
+++ src/components/questions/multiple-choice-question.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import classNames from "classnames";
 
-const MultipleChoiceQuestion = () => {
+const MultipleChoiceQuestion = ({question}) => {
+    const [curChoice, setCurChoice] = useState("")
+    const [graded, setGraded] = useState(false)
 
     return(
-        <div>Multiple Choice</div>
+        <div>
+            <h4>
+                {question.question}
+                {
+                    graded && curChoice === question.correct &&
+                    <span>
+                            &nbsp;
+                        <i className="fas fa-check"></i>
+                        </span>
+
+                }
+
+                {
+                    graded && curChoice !== question.correct &&
+                    <span>
+                            &nbsp;
+                        <i className="fas fa-times"></i>
+                        </span>
+
+                }
+            </h4>
+            <div className="list-group">
+                {
+                    question.choices.map( (choice) => {
+                            return(
+                                <div>
+                                    <div className={classNames("list-group-item",
+                                        {"list-group-item-success": graded && choice === question.correct,
+                                            "list-group-item-danger" : graded && curChoice !== question.correct && choice === curChoice})}>
+                                        <label><input
+                                            type="radio"
+                                            onClick={() => setCurChoice(choice)}
+                                            name={question._id}/>{choice}</label>
+                                    </div>
+                                </div>
+                            )
+                        })
+                }
+            </div>
+            <br/>
+            <div>
+                <h6>Your Answer: {curChoice}</h6>
+                <button className="btn btn-primary"
+                        onClick={() => setGraded(true)}>
+                    Grade
+                </button>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/questions/true-false-question.js
+++ src/components/questions/true-false-question.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import classNames from "classnames";
 
 const TrueFalseQuestion = ({question}) => {
-    const [choice, setChoice] = useState("")
+    const [curChoice, setCurChoice] = useState("")
     const [graded, setGraded] = useState(false)
 
     return(
@@ -10,7 +10,7 @@ const TrueFalseQuestion = ({question}) => {
             <h4>
                 {question.question}
                 {
-                    graded && choice === question.correct &&
+                    graded && curChoice === question.correct &&
                         <span>
                             &nbsp;
                             <i className="fas fa-check"></i>
@@ -19,7 +19,7 @@ const TrueFalseQuestion = ({question}) => {
                 }
 
                 {
-                    graded && choice !== question.correct &&
+                    graded && curChoice !== question.correct &&
                         <span>
                             &nbsp;
                             <i className="fas fa-times"></i>
@@ -31,25 +31,25 @@ const TrueFalseQuestion = ({question}) => {
             <div className="list-group">
                 <div className={classNames("list-group-item",
                     {"list-group-item-success": graded && 'true' === question.correct,
-                        "list-group-item-danger" : graded && choice !== question.correct && choice === "true"})}>
+                        "list-group-item-danger" : graded && curChoice !== question.correct && curChoice === "true"})}>
                     <label><input
                         type="radio"
-                        onClick={() => setChoice("true")}
+                        onClick={() => setCurChoice("true")}
                         name={question._id}/>True</label>
                 </div>
                 <div className={classNames("list-group-item",
                     {"list-group-item-success": graded && 'false' === question.correct,
-                        "list-group-item-danger" : graded && choice !== question.correct && choice === "false"}
+                        "list-group-item-danger" : graded && curChoice !== question.correct && curChoice === "false"}
                         )}>
                     <label><input
                         type="radio"
-                        onClick={() => setChoice("false")}
+                        onClick={() => setCurChoice("false")}
                         name={question._id}/>False</label>
                 </div>
             </div>
             <br/>
             <div>
-                <h6>Your Answer: {choice}</h6>
+                <h6>Your Answer: {curChoice}</h6>
                 <button className="btn btn-primary"
                         onClick={() => setGraded(true)}>
                     Grade
