0c26545d38b5878a326912f80d9a2d1ae9f93761
sravanikumar <kumar.sra@northeastern.edu>
2021-04-12 15:05:53 -0400

    added empty quizzes and questions service

--- src/components/quizzes/quizzes-list.js
+++ src/components/quizzes/quizzes-list.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const QuizzesList = () => {
+    const [quizzes, setQuizzes] = useState([])
+
+
     return(
         <div>
             <h1>Quizzes List</h1>
