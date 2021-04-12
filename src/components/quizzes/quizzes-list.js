70ea69705b7a6ab83e12a10ae6e0bcae799e42a4
sravanikumar <kumar.sra@northeastern.edu>
2021-04-12 15:24:51 -0400

    rendered quizzes list component

--- src/components/quizzes/quizzes-list.js
+++ src/components/quizzes/quizzes-list.js
@@ -1,12 +1,31 @@
 import React, {useState, useEffect} from "react";
+import quizService from "../../services/quizzes-service"
+import {Link, useParams} from "react-router-dom";
 
 const QuizzesList = () => {
+    const {courseId} = useParams();
     const [quizzes, setQuizzes] = useState([])
 
+    useEffect( () => {
+        quizService.findAllQuizzes()
+            .then(quizzes => setQuizzes(quizzes))
+        }, [])
 
     return(
         <div>
             <h1>Quizzes List</h1>
+            <div className="list-group">
+                {
+                    quizzes.map((quiz) => {
+                        return (
+                            <Link className="list-group-item"
+                                  to={`/courses/${courseId}/quizzes/${quiz._id}`}>
+                                {quiz.title}
+                            </Link>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
