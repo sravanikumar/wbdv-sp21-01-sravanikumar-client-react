f5954af01cb045c4b7ed99afd65079310c0cb576
sravanikumar <kumar.sra@northeastern.edu>
2021-04-19 22:35:07 -0400

    created redux store for quizzes

--- src/App.js
+++ src/App.js
@@ -5,39 +5,55 @@ import {BrowserRouter, Route} from "react-router-dom";
 import CourseEditor from "./components/course-editor/course-editor";
 import QuizzesList from "./components/quizzes/quizzes-list";
 import Quiz from "./components/quizzes/quiz";
+import {combineReducers, createStore} from "redux";
+import {Provider} from "react-redux";
+import quizReducer from "./reducers/quiz-reducer";
+import questionReducer from "./reducers/question-reducer";
+import quizAttemptReducer from "./reducers/quiz-attempt-reducer";
+
+const reducer = combineReducers({
+    quizReducer: quizReducer,
+    questionReducer: questionReducer,
+    quizAttemptReducer: quizAttemptReducer
+})
+
+const store = createStore(reducer)
+
 
 function App() {
   return (
-      <BrowserRouter>
-          <div className="container-fluid">
+      <Provider store={store}>
+          <BrowserRouter>
+              <div className="container-fluid">
 
-              <Route path="/" exact={true}>
-                  <Home/>
-              </Route>
+                  <Route path="/" exact={true}>
+                      <Home/>
+                  </Route>
 
-              <Route path={["/courses", "/courses/table", "/courses/grid"]} exact={true}>
-                  <CourseManager/>
-              </Route>
+                  <Route path={["/courses", "/courses/table", "/courses/grid"]} exact={true}>
+                      <CourseManager/>
+                  </Route>
 
-              <Route path={["/courses/:courseId/quizzes"]} exact={true}>
-                  <QuizzesList/>
-              </Route>
+                  <Route path={["/courses/:courseId/quizzes"]} exact={true}>
+                      <QuizzesList/>
+                  </Route>
 
-              <Route path={["/courses/:courseId/quizzes/:quizId"]} exact={true}>
-                  <Quiz/>
-              </Route>
+                  <Route path={["/courses/:courseId/quizzes/:quizId"]} exact={true}>
+                      <Quiz/>
+                  </Route>
 
-              <Route path={["/courses/:layout/edit",
-                            "/courses/:layout/edit/:courseId",
-                            "/courses/:layout/edit/:courseId/modules/:moduleId",
-                            "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId",
-                            "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId",
-                            "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId/widgets/:widgetType/:widgetId"
-                            ]}
-                     exact={true}
-                     render={(props) => <CourseEditor {...props}/>}/>
-          </div>
-      </BrowserRouter>
+                  <Route path={["/courses/:layout/edit",
+                                "/courses/:layout/edit/:courseId",
+                                "/courses/:layout/edit/:courseId/modules/:moduleId",
+                                "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId",
+                                "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId",
+                                "/courses/:layout/edit/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId/widgets/:widgetType/:widgetId"
+                                ]}
+                         exact={true}
+                         render={(props) => <CourseEditor {...props}/>}/>
+              </div>
+          </BrowserRouter>
+      </Provider>
   );
 }
 
