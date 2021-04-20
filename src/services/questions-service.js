bb02880123d919a8f3cd8bbad8fef077a1d7865b
sravanikumar <kumar.sra@northeastern.edu>
2021-04-19 20:54:02 -0400

    added submitQuiz service to quiz service

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -5,6 +5,19 @@ const findQuestionsForQuiz = (qid) => {
         .then(response => response.json())
 }
 
+const submitQuiz = (qzid, questions) => {
+    return fetch(`${QUIZZES_URL}/${qzid}/attempts`,
+        {
+            method: 'POST',
+            body: JSON.stringify(questions),
+            headers: {
+                'content-type': 'application/json'
+            }
+    }).then(response => response.json())
+        .then(result => console.log(result))
+}
+
 export default {
-    findQuestionsForQuiz
+    findQuestionsForQuiz,
+    submitQuiz
 }
